078ec9944278a132de900373d2487de7f8e00e9e
San Learn <sanlearn3@gmail.com>
2025-02-13 18:25:28 +0700

    client: reinitialize project with Vite using TypeScript and install shadcn/ui

---
+++ client/src/application.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+export function Application() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <Button>Hello World</Button>
+    </div>
+  );
+}
---
+++ client/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import { Application } from "./application";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Application />
+  </StrictMode>
+);
